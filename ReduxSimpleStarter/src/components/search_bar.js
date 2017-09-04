032111a029daa04a7fc915556626f0647f4d4ce3
Daniel <danielpolefko@gmail.com>
2017-09-04 19:40:35 -0400

    update src folder

---
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -0,0 +1,29 @@
+// AIzaSyDqzNREyArz5KVJeGcooFJBVFle6pQZ204
+import React, {Component} from 'react';
+
+class SearchBar extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {term: ''};
+    }
+    render() {
+        return (
+        <div>
+            <input onChange={event => this.setState({term: event.target.value})} />
+            Value of the input: {this.state.term}
+        </div>
+        );
+    }
+
+   // onInputChange(event){
+  //      console.log(event.target.value);
+ //   }
+}
+
+
+// const SearchBar = () => {
+ //   return <input />
+// };
+
+export default SearchBar;
